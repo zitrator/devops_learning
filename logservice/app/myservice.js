c4ba8ce6477e6511b536af4fc6e5abc7a7c3a23d
zitrator <zitrator@yahoo.com>
2022-04-11 23:47:37 +0300

    yes httpd

--- logservice/app/myservice.js
+++ logservice/app/myservice.js
@@ -3,12 +3,19 @@
  */
 
 
+let fs = require('fs');
 let express = require('express');
 let app = express();
 let fname = process.env.LOG_FNAME;
 
 app.get('/', function (req, res) {
-    res.send("hello world " + toString(fname));
+    fs.readFile('/common_logs/man.txt', 'utf8', function(err, data) {
+        if (err) 
+            throw err;
+        console.log('OK: ');
+        console.log(data)
+        res.send(data);
+    });   
 });
 
 app.listen(3000, function () {
